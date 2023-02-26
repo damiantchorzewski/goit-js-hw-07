96a71f8b24a69c8cadcbe7e16bbb379e6f6b2dd7
Damian Tchorzewski <damiantchorzewski0@gmail.com>
2023-02-27 00:12:49 +0100

    done

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const items = galleryItems.map(({ preview, original, description }) => {
+  const galleryItem = document.createElement('a');
+  galleryItem.classList.add('gallery__item');
+  galleryItem.setAttribute('href', original);
 
+  const image = document.createElement('img');
+  image.src = preview;
+  image.alt = description;
+  image.classList.add('gallery__image');
+
+  galleryItem.appendChild(image);
+
+  return galleryItem;
+});
+
+gallery.append(...items);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+});
 console.log(galleryItems);
