ab5d014d2d088c11de36139d99f36b1b5ae44334
Damian Tchorzewski <damiantchorzewski0@gmail.com>
2023-02-26 20:09:50 +0100

    edit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
+const createGalleryItem = ({ preview, original, description }) => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = original;
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = preview;
+  galleryImage.alt = description;
+  galleryImage.dataset.source = original;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+};
+
+const galleryItems = galleryItems.map(createGalleryItem);
+gallery.append(...galleryItems);
+
+gallery.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  });
+});
 console.log(galleryItems);
